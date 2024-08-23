07d754a99e33a4adc7de7dfa5eb705d6edea8bbe
CyberRoms <romualdojrb@yahoo.com>
2024-08-23 11:54:57 +0400

    .

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@ Built to seamlessly integrate with FIRENOR's documentation style, the Firenor Th
     'license': 'LGPL-3',
 
     # any module necessary for this one to work correctly
-    'depends': ['theme_common'],
+    'depends': ['website'],
 
     # always loaded
     'data': [
@@ -29,17 +29,17 @@ Built to seamlessly integrate with FIRENOR's documentation style, the Firenor Th
         # 'views/templates.xml',
     ],
     'assets': {
-        # 'web._assets_primary_variables': [
-        #     ('prepend', 'website_firenor/static/src/scss/primary_variables.scss'),
+        'web._assets_primary_variables': [
+            ('prepend', 'website_firenor/static/src/scss/primary_variables.scss'),
+        ],
+        # 'web.assets_backend': [
+        #     'website_firenor/static/src/scss/primary_variables.scss',
         # ],
-        'web.assets_backend': [
-            'website_firenor/static/src/scss/primary_variables.scss',
-        ],
-        'web.assets_frontend': [
-            'website_firenor/static/src/scss/primary_variables.scss',
-        ],
-        'website.assets_editor': [
-            'website_firenor/static/src/scss/primary_variables.scss',
-        ],
+        # 'web.assets_frontend': [
+        #     'website_firenor/static/src/scss/primary_variables.scss',
+        # ],
+        # 'website.assets_editor': [
+        #     'website_firenor/static/src/scss/primary_variables.scss',
+        # ],
     },
 }
