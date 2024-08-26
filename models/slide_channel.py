b778e7dd5e41fd9b5fdd8aa079f3d8b0f2828450
CyberRoms <romualdojrb@yahoo.com>
2024-08-26 09:48:48 +0400

    .

--- models/slide_channel.py
+++ models/slide_channel.py
@@ -8,8 +8,12 @@ class FN_Channel(models.Model):
         res = super(FN_Channel, self)._default_cover_properties()
         # Update the background image with the new gradient
         res.update({
-            'background_image': (
+            "background_color_class": "o_cc3",
+            'background_color_style': (
+                'background-color: rgba(0, 0, 0, 0); '
                 'background-image: linear-gradient(120deg, #47a5ae, #47a5ae) !important;'
             ),
+            'opacity': '0',
+            'resize_class': 'cover_auto'
         })
         return res
