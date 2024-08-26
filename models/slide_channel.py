5fabcbd3474ca9df498ec208f5f2d4705bad3dee
CyberRoms <romualdojrb@yahoo.com>
2024-08-26 12:45:51 +0400

    .

--- models/slide_channel.py
+++ models/slide_channel.py
@@ -8,13 +8,9 @@ class SlideChannel(models.Model):
         res = super()._default_cover_properties()  # Correct class name here
         # Update the background image with the new gradient
         res.update({
-            "background_color_class": "o_cc3",
             'background_color_style': (
-                'background-color: rgba(0, 0, 0, 0); '
                 'background-image: linear-gradient(120deg, #47a5ae, #47a5ae) !important;'
             ),
-            'opacity': '0',
-            'resize_class': 'cover_auto'
         })
         return res
 
