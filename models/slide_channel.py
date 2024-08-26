17cfc1793dd91ca7c25fbfbd78caa3723ba8126d
CyberRoms <romualdojrb@yahoo.com>
2024-08-26 10:01:15 +0400

    .

--- models/slide_channel.py
+++ models/slide_channel.py
@@ -1,11 +1,11 @@
 from odoo import _, api, fields, models
 
-class FN_Channel(models.Model):
+class SlideChannel(models.Model):
     _inherit = 'slide.channel'
 
     def _default_cover_properties(self):
         """Override the default cover properties to change the background image gradient."""
-        res = super(FN_Channel, self)._default_cover_properties()
+        res = super(SlideChannel, self)._default_cover_properties()  # Use correct class name
         # Update the background image with the new gradient
         res.update({
             "background_color_class": "o_cc3",
