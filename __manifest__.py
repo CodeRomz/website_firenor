8fa4ff76796ed0e6a2e08cd512b5775db2b62ad3
CyberRoms <romualdojrb@yahoo.com>
2024-08-26 14:19:46 +0400

    .

--- __manifest__.py
+++ __manifest__.py
@@ -30,17 +30,17 @@ Built to seamlessly integrate with FIRENOR's documentation style, the Firenor Th
         # 'data/theme_data.xml',
     ],
     'assets': {
-        # 'web._assets_primary_variables': [
-        #     ('prepend', 'website_firenor/static/src/scss/primary_variables.scss'),
+        'web._assets_primary_variables': [
+            ('prepend', 'website_firenor/static/src/scss/primary_variables.scss'),
+        ],
+        # 'web.assets_backend': [
+        #     'website_firenor/static/src/scss/firenor_style.scss',
         # ],
-        'web.assets_backend': [
-            'website_firenor/static/src/scss/firenor_style.scss',
-        ],
-        'web.assets_frontend': [
-            'website_firenor/static/src/scss/firenor_style.scss',
-        ],
-        'website.assets_editor': [
-            'website_firenor/static/src/scss/firenor_style.scss',
-        ],
+        # 'web.assets_frontend': [
+        #     'website_firenor/static/src/scss/firenor_style.scss',
+        # ],
+        # 'website.assets_editor': [
+        #     'website_firenor/static/src/scss/firenor_style.scss',
+        # ],
     },
 }
